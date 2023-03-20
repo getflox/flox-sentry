b6207a5a5e81bc475706448feefd4d57f0a10772
Daniel Ancuta <whisller@gmail.com>
2023-03-20 12:58:19 +0100

    Allow to set `resolveAge`.

--- flox_sentry/project.py
+++ flox_sentry/project.py
@@ -13,7 +13,10 @@ def create_project(flox: FloxContext, sentry: SentryApi, output, **kwargs):
         project_slug=flox.project.id,
         team_slug=flox.profile.sentry.team.slug,
         project=dict(
-            name=flox.project.name, slug=flox.project.id, default_rules=False if flox.profile.sentry.rules else True
+            name=flox.project.name,
+            slug=flox.project.id,
+            default_rules=False if flox.profile.sentry.rules else True,
+            resolveAge=flox.profile.sentry.get("autoResolve", None),  # Value in hours
         ),
     )
 
@@ -27,8 +30,10 @@ def create_alerts(flox: FloxContext, sentry: SentryApi, output, **kwargs):
 
     existing_rules = sentry.project_rules.all(project_slug=flox.project.id).json()
     for rule in flox.profile.sentry.rules:
+        rule_name = rule.get("name").replace("{project_id}", flox.project.id)
+
         rule_exists = next(
-            iter(filter(lambda existing_rule: existing_rule.get("name") == rule.get("name"), existing_rules)), None
+            iter(filter(lambda existing_rule: existing_rule.get("name") == rule_name, existing_rules)), None
         )
         if not rule_exists:
             sentry.project_rules.create(project_slug=flox.project.id, rule=rule)
